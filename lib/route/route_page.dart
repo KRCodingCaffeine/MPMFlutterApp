c42168b0ad29289eadf327d165c2f66def00edae
ManojKumar <manojkumarmurugan76@gmail.com>
2025-02-21 15:59:09 +0530

    route file changes

--- lib/route/route_page.dart
+++ lib/route/route_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:mpm/route/route_name.dart';
 import 'package:mpm/view/SearchView.dart';
 import 'package:mpm/view/addmember/add_member_first.dart';
@@ -12,6 +11,7 @@ import 'package:mpm/view/dashboard_view.dart';
 import 'package:mpm/view/discount_offer_view.dart';
 import 'package:mpm/view/forms_down.dart';
 import 'package:mpm/view/gov_scheme.dart';
+import 'package:mpm/view/home_view.dart';
 import 'package:mpm/view/language_view.dart';
 import 'package:mpm/view/login_view.dart';
 import 'package:mpm/view/otp_view.dart';
@@ -23,42 +23,43 @@ import 'package:mpm/view/samiti%20members/samiti_detail_view.dart';
 import 'package:mpm/view/samiti%20members/samiti_members_view.dart';
 import 'package:mpm/view/splash_view.dart';
 
-
 class RoutePages {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case RouteNames.splash_screen:
-        return _buildRoute(SplashView(),settings);
+        return _buildRoute(SplashView(), settings);
       case RouteNames.login_screen:
-        return _buildRoute(LoginPage(),settings);
-        case RouteNames.language_screen:
-        return _buildRoute(LanguageSelectionPage(),settings);
-        case RouteNames.otp_screen:
-        return _buildRoute(OTPScreen(),settings);
+        return _buildRoute(LoginPage(), settings);
+      case RouteNames.language_screen:
+        return _buildRoute(LanguageSelectionPage(), settings);
+      case RouteNames.otp_screen:
+        return _buildRoute(OTPScreen(), settings);
       case RouteNames.registration_screen:
-        return _buildRoute(RegisterView(),settings);
-        case RouteNames.personalinfo:
-        return _buildRoute(PersonalView(),settings);
-        case RouteNames.residentailinfo:
-        return _buildRoute(PesidentalAdressView(),settings);
-        case RouteNames.dashboard:
-        return _buildRoute(DashboardView(),settings);
-     case RouteNames.newMember:
-        return _buildRoute(AddNewMemberFirst(),settings);
-        case RouteNames.newMember2:
-        return _buildRoute(NewMemberResidental(),settings);
-        case RouteNames.aboutUs:
-        return _buildRoute(AboutViewPage(),settings);
-        case RouteNames.contactUs:
-        return _buildRoute(ContactViewPage(),settings);
-        case RouteNames.pravacypolicy:
-        return _buildRoute(PrivacyPolicyViewPage(),settings);
-        case RouteNames.termandcondition:
-        return _buildRoute(TermsconditionViewPage(),settings);
-        case RouteNames.samitimemberview:
-        return _buildRoute(SamitiMembersViewPage(),settings);
-        case RouteNames.samitimemberdetails:
-        return _buildRoute(SamitiDetailPage(),settings);
+        return _buildRoute(RegisterView(), settings);
+      case RouteNames.personalinfo:
+        return _buildRoute(PersonalView(), settings);
+      case RouteNames.residentailinfo:
+        return _buildRoute(PesidentalAdressView(), settings);
+      case RouteNames.home_screen:
+        return _buildRoute(const HomeView(), settings);
+      case RouteNames.dashboard:
+        return _buildRoute(DashboardView(), settings);
+      case RouteNames.newMember:
+        return _buildRoute(AddNewMemberFirst(), settings);
+      case RouteNames.newMember2:
+        return _buildRoute(NewMemberResidental(), settings);
+      case RouteNames.aboutUs:
+        return _buildRoute(AboutViewPage(), settings);
+      case RouteNames.contactUs:
+        return _buildRoute(ContactViewPage(), settings);
+      case RouteNames.pravacypolicy:
+        return _buildRoute(PrivacyPolicyViewPage(), settings);
+      case RouteNames.termandcondition:
+        return _buildRoute(TermsconditionViewPage(), settings);
+      case RouteNames.samitimemberview:
+        return _buildRoute(SamitiMembersViewPage(), settings);
+      case RouteNames.samitimemberdetails:
+        return _buildRoute(SamitiDetailPage(), settings);
       case RouteNames.discount_offer_view:
         return _buildRoute(const DiscountofferView(), settings);
       case RouteNames.forms:
@@ -68,17 +69,19 @@ class RoutePages {
       case RouteNames.profile:
         return _buildRoute(const ProfileView(), settings);
       case RouteNames.searchmember:
-        return _buildRoute(SearchView(),settings);
-        default:
+        return _buildRoute(SearchView(), settings);
+      default:
         return MaterialPageRoute(builder: (context) {
           return Scaffold(
-            body: Center(child: Text("No Routes Declare"),
+            body: Center(
+              child: Text("No Routes Declare"),
             ),
           );
         });
     }
   }
-  static PageRouteBuilder _buildRoute(Widget page,RouteSettings setting) {
+
+  static PageRouteBuilder _buildRoute(Widget page, RouteSettings setting) {
     return PageRouteBuilder(
       settings: setting,
       pageBuilder: (context, animation, secondaryAnimation) => page,
@@ -87,7 +90,8 @@ class RoutePages {
         const end = Offset.zero;
         const curve = Curves.easeInOut;
 
-        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
         var offsetAnimation = animation.drive(tween);
 
         return SlideTransition(
