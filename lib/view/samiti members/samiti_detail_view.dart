55eb4598c01051ece77d6c6902c1bb04104327f3
KR Coding Caffeine <krcodingcaffeine@gmail.com>
2025-02-22 13:32:37 +0530

    Place holder image added in network image

--- lib/view/samiti members/samiti_detail_view.dart
+++ lib/view/samiti members/samiti_detail_view.dart
@@ -9,12 +9,13 @@ import '../../utils/urls.dart';
 
 class SamitiDetailPage extends StatefulWidget {
   SamitiDetailPage({super.key});
+
   @override
   _SamitiDetailPageState createState() => _SamitiDetailPageState();
 }
 
 class _SamitiDetailPageState extends State<SamitiDetailPage> {
-  SamitiController controller= Get.put(SamitiController());
+  SamitiController controller = Get.put(SamitiController());
 
   @override
   void initState() {
@@ -28,85 +29,76 @@ class _SamitiDetailPageState extends State<SamitiDetailPage> {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        title: Text(controller.samitiName.value.toString(), style: TextStyle(color: Colors.white),),
-        backgroundColor: ColorHelperClass.getColorFromHex(ColorResources.logo_color),
+        title: Text(
+          controller.samitiName.value.toString(),
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor:
+            ColorHelperClass.getColorFromHex(ColorResources.logo_color),
         iconTheme: IconThemeData(color: Colors.white),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Obx((){
-          if(controller.loading2.value)
-            {
-              return Center(child: CircularProgressIndicator(),);
-            }
-          if(controller.samitiDetailList.value==null || controller.samitiDetailList.value.isEmpty)
-          {
-            return SafeArea(child: Center(child:Text("No data found "),));
-          }
-          return ListView.builder(
-            itemCount: controller.samitiDetailList.value.length,
-            itemBuilder: (context, index) {
-              final member = controller.samitiDetailList.value[index];
-              print("member samiti ${member.memberCode}");
-              var firstname="";
-              var middlename="";
-              var lastname="";
-              var mobile="";
-              var memberCode="";
-              if(member.firstName.toString()=="null")
-                {
-                  firstname="";
-                }
-              else
-                {
-                 firstname= member.firstName.toString();
-                }
-              if(member.middleName.toString()=="null")
-                {
-                  middlename="";
-                }
-              else
-                {
-                 middlename= member.middleName.toString();
-                }
-              if(member.lastName.toString()=="null")
-                {
-                  lastname="";
-                }
-              else
-                {
-                 lastname= member.lastName.toString();
-                }
-              if(member.mobile.toString()=="null")
-                {
-                  mobile= " ";
-                }
-              else
-                {
-                 mobile= member.mobile.toString();
-                }
-              var name =firstname+" "+lastname;
-
-              if(member.memberCode.toString()=="null")
-              {
-                memberCode="";
-              }
-              else
-              {
-                memberCode= member.memberCode.toString();
-              }
-              return _buildMemberCard(
-                samitiRoles: member.samitiRolesName.toString(),
-                lmCode: memberCode,
-               name: name,
-                mobile: mobile,
-                profileImage: member.profileImagePath,
-                memberId: member.memberId
+          padding: const EdgeInsets.all(16.0),
+          child: Obx(() {
+            if (controller.loading2.value) {
+              return Center(
+                child: CircularProgressIndicator(),
               );
-            },
-          );
-        })
-      ),
+            }
+            if (controller.samitiDetailList.value == null ||
+                controller.samitiDetailList.value.isEmpty) {
+              return SafeArea(
+                  child: Center(
+                child: Text("No data found "),
+              ));
+            }
+            return ListView.builder(
+              itemCount: controller.samitiDetailList.value.length,
+              itemBuilder: (context, index) {
+                final member = controller.samitiDetailList.value[index];
+                print("member samiti ${member.memberCode}");
+                var firstname = "";
+                var middlename = "";
+                var lastname = "";
+                var mobile = "";
+                var memberCode = "";
+                if (member.firstName.toString() == "null") {
+                  firstname = "";
+                } else {
+                  firstname = member.firstName.toString();
+                }
+                if (member.middleName.toString() == "null") {
+                  middlename = "";
+                } else {
+                  middlename = member.middleName.toString();
+                }
+                if (member.lastName.toString() == "null") {
+                  lastname = "";
+                } else {
+                  lastname = member.lastName.toString();
+                }
+                if (member.mobile.toString() == "null") {
+                  mobile = " ";
+                } else {
+                  mobile = member.mobile.toString();
+                }
+                var name = firstname + " " + lastname;
+
+                if (member.memberCode.toString() == "null") {
+                  memberCode = "";
+                } else {
+                  memberCode = member.memberCode.toString();
+                }
+                return _buildMemberCard(
+                    samitiRoles: member.samitiRolesName.toString(),
+                    lmCode: memberCode,
+                    name: name,
+                    mobile: mobile,
+                    profileImage: member.profileImagePath,
+                    memberId: member.memberId);
+              },
+            );
+          })),
     );
   }
 
@@ -119,12 +111,6 @@ class _SamitiDetailPageState extends State<SamitiDetailPage> {
     required String profileImage,
     required String memberId,
   }) {
-    // Use the default image if profileImage is null or empty
-    String imagePath = (profileImage.isNotEmpty && profileImage != null)
-        ? profileImage
-        : 'assets/images/user3.png'; // Default image path
-    print('Profile Image Path: $imagePath');
-
     return Center(
       child: Card(
         color: Colors.white,
@@ -138,10 +124,26 @@ class _SamitiDetailPageState extends State<SamitiDetailPage> {
               // Profile Image with default fallback
               CircleAvatar(
                 radius: 40,
-                backgroundImage: (profileImage != null && profileImage.isNotEmpty)
-                    ? NetworkImage(Urls.imagePathUrl + profileImage)
-                    : const AssetImage("assets/images/male.png") as ImageProvider,
                 backgroundColor: Colors.grey[300],
+                // Background color while loading
+                child: ClipOval(
+                  child: (profileImage != null && profileImage.isNotEmpty)
+                      ? FadeInImage(
+                          placeholder:
+                              const AssetImage("assets/images/user3.png"),
+                          // Placeholder while loading
+                          image: NetworkImage(Urls.imagePathUrl + profileImage),
+                          imageErrorBuilder: (context, error, stackTrace) {
+                            return Image.asset("assets/images/male.png",
+                                fit: BoxFit.cover);
+                          },
+                          fit: BoxFit.cover,
+                          width: 80,
+                          height: 80,
+                        )
+                      : Image.asset("assets/images/male.png",
+                          fit: BoxFit.cover, width: 80, height: 80),
+                ),
               ),
               const SizedBox(width: 16),
               // Information Section
